fdf161a97eb89292f273d3db26b7b5d152cc8e93
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-03 15:26:26 -0400

    Updates

---
+++ precache-manifest.697b482202189bf953b1ce7ebcd27bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf851c7782ace2a5aaa7a3eb1af7f757",
+    "url": "/clickCounter/index.html"
+  },
+  {
+    "revision": "db9be9c93dfb0bf11bc7",
+    "url": "/clickCounter/static/css/main.6ecf1d06.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickCounter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "db9be9c93dfb0bf11bc7",
+    "url": "/clickCounter/static/js/main.961fd015.chunk.js"
+  },
+  {
+    "revision": "462af49eb06727e0638b",
+    "url": "/clickCounter/static/js/runtime~main.c0313f98.js"
+  }
+]);
