1276deb0990db9b7d9225a62986a54d97294bbf8
Domenico Manna <mannadomenico2849@gmail.com>
2019-05-03 14:59:36 -0400

    Add clicker functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header/Header';
 import Clicker from './Clicker/Clicker';
 
-function App() {
-  return (
-    <div>
-        <Header/>
-        <Clicker/>
-    </div>
-  );
+const startingClickerValue = 0;
+
+class App extends Component {
+    state = {
+        clickerValue: startingClickerValue
+    }
+
+    incrementValue() {
+        this.setState({
+            clickerValue: this.state.clickerValue + 1
+        })
+    }
+
+    decrementValue() {
+        this.setState({
+            clickerValue: this.state.clickerValue - 1
+        })
+    }
+
+    resetValue() {
+        this.setState({
+            clickerValue: startingClickerValue
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Clicker value={this.state.clickerValue} incrementValue={this.incrementValue.bind(this)}
+                    decrementValue={this.decrementValue.bind(this)} resetValue={this.resetValue.bind(this)} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Clicker/Clicker.js
+++ src/Clicker/Clicker.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Clicker.css';
 
-export default function Clicker() {
+export default function Clicker(props) {
     return (
         <section className="clicker">
             <div className="clicker__wrapper">
-                <p className="clicker__value"> 0 </p>
+                <p className="clicker__value"> {props.value} </p>
                 <div className="clicker__buttons">
-                    <button className="button clicker__minus"> <i className="fas fa-minus"></i> </button>
-                    <button className="button clicker__reset"> <i className="fas fa-undo"></i> </button>
-                    <button className="button clicker__plus"> <i className="fas fa-plus"></i> </button>
+                    <button onClick = {props.decrementValue} className="button clicker__minus"> <i className="fas fa-minus"></i> </button>
+                    <button onClick = {props.resetValue} className="button clicker__reset"> <i className="fas fa-undo"></i> </button>
+                    <button onClick = {props.incrementValue} className="button clicker__plus"> <i className="fas fa-plus"></i> </button>
                 </div>
             </div>
         </section>
